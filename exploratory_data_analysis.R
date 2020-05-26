76001f024921c0e30547cc674e93516391ed50f2
Tanvi Jain <60771234+tanvijain09@users.noreply.github.com>
2020-05-26 19:53:11 +0530

    Add files via upload

---
+++ exploratory_data_analysis.R
@@ -0,0 +1,44 @@
+setwd("~/Desktop/BigDataAnalytics/ExData_Plotting1")
+downloadURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+downloadFile <- "./Data/household_power_consumption.zip"
+householdFile <- "./Data/household_power_consumption.txt"
+##
+if (!file.exists(householdFile)) {
+  download.file(downloadURL, downloadFile, method = "curl")
+  unzip(downloadFile, overwrite = T, exdir = "./Data")
+}
+##
+plotData <- read.table(householdFile, header=T, sep=";", na.strings="?")
+## set time variable
+finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalData <- cbind(SetTime, finalData)
+##
+## Generating Plot 1
+hist(finalData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+## Generating Plot 2
+
+plot(finalData$SetTime, finalData$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power (kilowatts)")
+
+## Generating Plot 3
+
+columnlines <- c("black", "red", "blue")
+labels <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+plot(finalData$SetTime, finalData$Sub_metering_1, type="l", col=columnlines[1], xlab="", ylab="Energy sub metering")
+lines(finalData$SetTime, finalData$Sub_metering_2, col=columnlines[2])
+lines(finalData$SetTime, finalData$Sub_metering_3, col=columnlines[3])
+legend("topright", legend=labels, col=columnlines, lty="solid")
+
+
+## Generating Plot 4
+labels <- c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
+columnlines <- c("black","red","blue")
+par(mfrow=c(2,2))
+plot(finalData$SetTime, finalData$Global_active_power, type="l", col="green", xlab="", ylab="Global Active Power")
+plot(finalData$SetTime, finalData$Voltage, type="l", col="orange", xlab="datetime", ylab="Voltage")
+plot(finalData$SetTime, finalData$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(finalData$SetTime, finalData$Sub_metering_2, type="l", col="red")
+lines(finalData$SetTime, finalData$Sub_metering_3, type="l", col="blue")
+legend("topright", bty="n", legend=labels, lty=1, col=columnlines)
+plot(finalData$SetTime, finalData$Global_reactive_power, type="l", col="blue", xlab="datetime", ylab="Global_reactive_power")
